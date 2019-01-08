af5b0d81c9f207c1d3d034e8f8fc7141146c5326
Matthew Lefaive <lefaivem@gmail.com>
2019-01-08 17:59:27 -0500

    Fixed minor issue with sets vs lists not working properly between python versions

--- species_link.py
+++ species_link.py
@@ -24,34 +24,21 @@ except NameError:
 
 ### MAIN SPECIES LINK CODE ###
 def insertSpeciesLinks(text):
-	''' (str) -> str
-	Given input text, will replace all occurences of species listed in external file
-	'common_species.txt' with the corresponding species link. 
-
-	Insertion of species links follows Bioline standards for link insertion:
-	1. Only the first occurence out of both the abstract and title gets a link
-	2. Subsequent occurences are italicized
-	3. Each different language abstract/title will receive its own links
-	4. Species names in keywords are ignored
-
-	The text with species links and italicized species names will be returned.
-	'''
 	# Read in common species
 	f = open("./common_species.txt")
 	species_list = f.read().splitlines()
 	f.close()
 
-	# Split text as we are only editing text from the first title to last abstract (main_body)
+	# Split text as we are only considering the first title to last abstract (main_body)
 	pre_title = text[:text.index("<title")]
 	main_body = text[text.index("<title"):text.rindex("<keyword")]
 	post_abstract = text[text.rindex("<keyword"):]
 
-	# Get indices for each language from <title to </abstract>. Only within these sections
-	# can species links be inserted.
+	# Get indices for each different language body stuff (title-/abstract for each language)
 	start_title_indices = [m.start() for m in re.finditer("<title", main_body)]
 	end_abstract_indices = [m.start() for m in re.finditer("</abstract>", main_body)]
 
-	# Starting backwards from last language in the xml document
+	# Move backwards from last language 
 	for j in range(len(start_title_indices) - 1, -1, -1):
 		# Clear lists cuz we're starting a new language
 		linked_species = []
@@ -65,12 +52,10 @@ def insertSpeciesLinks(text):
 			index = 0
 			while index != -1:
 				try:
-					# Find index of first occurence of species
 					index = body[index:].lower().index(species.lower()) + index
 				except ValueError:
 					index = -1
 				else: # Index was found
-					# Split text into everything before and after the species
 					front_body = body[:index]
 					end_body = body[index + len(species):]
 
@@ -86,7 +71,7 @@ def insertSpeciesLinks(text):
 						add_short_forms(short_forms, species.lower())
 
 
-					# Not first occurrence, italicize if not in a species link tag, and not italicized.
+					# Not first occurrence, italicize if not in a species link tag and not italicized.
 					else:
 						if not in_sp_tag(body, index) and not is_italicized(body, index):
 							insert = "<i>" + case_sens_species + "</i>"
@@ -100,7 +85,6 @@ def insertSpeciesLinks(text):
 		# Go through body once more and italicize short forms for linked species
 		for sf in short_forms:
 			body = body.replace(sf, "<i>" + sf + "</i>")
-			body = body.replace(sf.lower(), "<i>" + sf.lower() + "</i>")
 	
 		# Rejoin body to end of main_body
 		main_body = main_body[:start_title_indices[j]] + body + main_body[end_abstract_indices[j]:]
@@ -111,10 +95,6 @@ def insertSpeciesLinks(text):
 					
 
 def in_sp_tag(body, index):
-	''' (str, int) -> bool
-	Returns true if the string body ends with the beginning of a species link, with body
-	being truncated at supplied index.
-	'''
 	if (body[:index].endswith("<sp>")):
 		return True
 	elif (body[:index].endswith("genus=\"")):
@@ -123,48 +103,24 @@ def in_sp_tag(body, index):
 		return False
 
 def is_italicized(body, index):
-	''' (str, int) -> bool
-	Returns true if the string body ends with <i> at the supplied index
-
-	>>> is_italicized("abc<i>", 3)
-	True
-	'''
 	return body[:index].endswith("<i>")
 
 def is_species_link(text):
-	''' (str) -> bool
-	Returns True is the supplied text is a species link
-
-	>>> is_species_link("<taxon genus="Arundo" species="donax" sub-prefix="" sub-species=""><sp>Arundo</sp> <sp>donax</sp></taxon>")
-	True
-	>>> is_species_link("Arundo donax")
-	False
-	'''
 	return re.match(r'^<taxon genus=".*" species=".*" sub-prefix=".*" sub-species=".*">.*<\/taxon>$', text)
 
 def remove_species_link(text):
-	''' (str) -> str
-	Will revert all species links in text with the species names to which they link
-	'''
 	text = re.sub(r'<taxon genus=".*" species=".*" sub-prefix=".*" sub-species=".*">', '', text)
 	text = re.sub(r'<\/?sp>', '', text)
 	text = text.replace("</taxon>", "")
 	return text
 
 def add_short_forms(short_forms, species):
-	''' ([str], str) -> null
-	Adds short form of species to short_forms list. A short form consists of Genus initial
-	followed by subspecies.
-	Ex. Arundo donax -> A. donax
-	'''
 	components = species.split(" ")
 	if len(components) > 1:
-		short_forms += [components[0][0].upper() + ". " + " ".join(components[1:])]
+		short_forms += [components[0][0] + ". " + " ".join(components[1:])]
 
 def get_species_link(link):
-	''' (str) -> str
-	Given the string link, returns a species link
-	'''
+	#return "sp(" + species + ")"
 
 	tokens = link.split()
 
@@ -201,33 +157,7 @@ def get_species_link(link):
 
 	return link
 				
-def is_parenthetical_match(s1, s2):
-	'''
-	Returns True if s2 = ^s1$, where ^ and $ can be any character. False otherwise. Comparison is case-insensitive.
 
-	>>> is_parenthetical_match("ham", "(ham)")
-	True
-	>>> is_parenthetical_match("X", "aXb")
-	True
-	'''
-	if len(s2) != len(s1) + 2:
-		return False
-	else:
-		return s1.lower() == s2[1:len(s2)-1].lower()
-
-def is_parenthetical(s):
-	'''
-	Returns True if s starts with ( and ends with )
-	'''
-	if len(s) < 2:
-		return False
-	else:
-		return s[0] == "(" and s[-1] == ")"
-
-
-### SPECIES LINKS ###
-# The following functions each return a species link and cover the wide variety of
-# formats a species name can be supplied in.
 def genus_spp(tokens):
 	'''
 	Input:  Brassica
@@ -300,6 +230,23 @@ def genus_species_MERGE_subspecies(tokens):
 	link += "<sp>" + genus + "</sp> <sp>" + species + "</sp> " + merge + " <sp>" + subspecies + "</sp></taxon>"
 	return link
 
-if __name__ == "__main__":
-	f = open("C:/Users/Mathew Lefaive/Desktop/test/ja21(7)/xml/ja17153.xml")
-	print(insertSpeciesLinks(f.read()))
+def is_parenthetical_match(s1, s2):
+	'''
+	Returns True if s2 = ^s1$, where ^ and $ can be any character. False otherwise. Comparison is case-insensitive.
+
+	>>> is_parenthetical_match("ham", "(ham)")
+	True
+	'''
+	if len(s2) != len(s1) + 2:
+		return False
+	else:
+		return s1.lower() == s2[1:len(s2)-1].lower()
+
+def is_parenthetical(s):
+	'''
+	Returns True if s starts with ( and ends with )
+	'''
+	if len(s) < 2:
+		return False
+	else:
+		return s[0] == "(" and s[-1] == ")"
